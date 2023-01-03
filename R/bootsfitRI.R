518a942d6306e01978e9da25179bd3bb48dfbc24
Shanpeng Li <shanpengli@rorronaldperez.ad.medctr.ucla.edu>
2023-01-02 21:37:46 -0800

    update codes

--- R/bootsfitRI.R
+++ R/bootsfitRI.R
@@ -56,6 +56,7 @@ bootsfitRI <- function(i, seed = 99, n = 100, phi = 0.04,
     coefSE <- rep(NA, 18)
     coef <- list(coef, coefSE)
     names(coef) <- c("coef", "coefSE")
+    return(coef)
   } else if (is.null(fit$beta)) {
     coef <- rep(NA, 20)
     coefSE <- rep(NA, 18)
