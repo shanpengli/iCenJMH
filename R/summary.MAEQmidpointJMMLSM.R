96aa5217880a31bea0d28d6f3ccd6633b5b24446
Shanpeng Li <shanpengli@SPMacBook.lan>
2023-09-16 16:16:39 -0700

    update bugs

--- R/summary.MAEQmidpointJMMLSM.R
+++ R/summary.MAEQmidpointJMMLSM.R
@@ -23,8 +23,7 @@ summary.MAEQmidpointJMMLSM <- function (object, digits = 3, ...) {
       
       sum[, -1] <- round(sum[, -1], digits)
       cat("\nSum of absolute error across quantiles of predicted risk scores at the landmark time of", object$landmark.time, "\nbased on", object$n.cv, "fold cross validation\n")
-      print(sum)
-      invisible(object)
+      return(sum)
     } else {
       stop("The cross validation fails. Please try using a different seed number.")
     }
